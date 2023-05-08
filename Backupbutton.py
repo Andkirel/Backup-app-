4e405d216c825a234c06695a4089490e180b0082
Andrew Ruiz <108295108+Andkirel@users.noreply.github.com>
2023-05-08 17:25:03 -0500

    Day Three

--- Backupbutton.py
+++ Backupbutton.py
@@ -9,12 +9,17 @@ class Backupbutton(customtkinter.CTk):
 
         self.Middle_frame = customtkinter.CTkFrame(self, width=850, height=500, corner_radius=10)
         self.Middle_frame.place(relx=0.585, rely=0.45, anchor=tkinter.CENTER)
+        self.Sub_frame = customtkinter.CTkFrame(self.Middle_frame, width=550, height=200, corner_radius=10)
+        self.Sub_frame.place(relx=0.5, rely=0.6, anchor=tkinter.CENTER)
 
         self.File_entry1 = customtkinter.CTkEntry(self.Middle_frame, width=450,height=25,corner_radius=10, placeholder_text="File Path", textvariable=str1)
         self.File_entry1.place(relx=0.5, rely=0.2, anchor=tkinter.CENTER)
 
         self.Multi_Files = customtkinter.CTkCheckBox(self.Middle_frame,width=25,height=25, text="Add another File")
-        self.Multi_Files.place(relx=0.85, rely=0.2, anchor=tkinter.CENTER)
+        self.Multi_Files.place(relx=0.87, rely=0.2, anchor=tkinter.CENTER)
+
+        self.Backup_Button = customtkinter.CTkButton(self.Middle_frame, text="Backup")
+        self.Backup_Button.place(relx=0.87, rely=0.9, anchor=tkinter.CENTER)
 
     #def Check_Box_event(self):
         #str2 = tkinter.StringVar()
