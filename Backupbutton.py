fde40234027545ec3dd2fce9d597adc41650b441
Andrew Ruiz <108295108+Andkirel@users.noreply.github.com>
2023-05-11 22:45:28 -0500

    Day one Week 2

--- Backupbutton.py
+++ Backupbutton.py
@@ -12,7 +12,7 @@ class Backupbutton(customtkinter.CTk):
         self.Sub_frame = customtkinter.CTkFrame(self.Middle_frame, width=550, height=200, corner_radius=10)
         self.Sub_frame.place(relx=0.5, rely=0.6, anchor=tkinter.CENTER)
 
-        self.File_entry1 = customtkinter.CTkEntry(self.Middle_frame, width=450,height=25,corner_radius=10, placeholder_text="File Path", textvariable=str1)
+        self.File_entry1 = customtkinter.CTkEntry(self.Middle_frame, width=450,height=25,corner_radius=10, placeholder_text="4 digit Pin", textvariable=str1)
         self.File_entry1.place(relx=0.5, rely=0.2, anchor=tkinter.CENTER)
 
         self.Multi_Files = customtkinter.CTkCheckBox(self.Middle_frame,width=25,height=25, text="Add another File")
@@ -21,8 +21,11 @@ class Backupbutton(customtkinter.CTk):
         self.Backup_Button = customtkinter.CTkButton(self.Middle_frame, text="Start Backup", width=170, height=50)
         self.Backup_Button.place(relx=0.87, rely=0.9, anchor=tkinter.CENTER)
 
-        self.progressbar = customtkinter.CTkProgressBar(self.Sub_frame, orientation="horizontal")
-        self.progressbar.place(relx=0.5, rely=0.5, anchor=tkinter.CENTER)
-        self.progressbar.configure(mode="determinate")
-        value = self.progressbar.get()
+        self.Zip_The_File = customtkinter.CTkCheckBox(self.Sub_frame, width=100, height=100, text="Compress File")
+        self.Zip_The_File.place(relx=0.7, rely=0.5, anchor=tkinter.CENTER)
+
+        self.Delete_Local_File = customtkinter.CTkCheckBox(self.Sub_frame, width=100, height=100, text="Delete the local files")
+        self.Delete_Local_File.place(relx=0.3, rely=0.5, anchor=tkinter.CENTER)
+
+
 
